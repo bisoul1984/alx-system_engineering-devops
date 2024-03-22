a1508fbec5ec5451da8ddb82aced68b8e83deb48
bisoul1984 <fikertetadesse1403@gmail.com>
2024-03-22 07:26:31 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-# Read: http://puppet-lint.com/checks/
-# Using Puppet, install puppet-lint.
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# This Puppet manifest installs Flask version 2.1.0 using pip3
+
+class flask_installation {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install Flask':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
+    require => Package['python3-pip'],
+  }
 }
+
+include flask_installation
+
