479875e985181adb49adcc722af54d3b0bdf0829
bisoul1984 <fikertetadesse1403@gmail.com>
2024-05-07 10:37:17 -0700

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of total subscribers for a
     given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Python/1.0 (Alx School 0x16 task 0)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Alx School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-# Test the function
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        print(number_of_subscribers(subreddit))
-
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
